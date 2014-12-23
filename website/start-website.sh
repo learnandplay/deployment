627034b7632726066f3da07c5d4ad09b9453140c
zanchi-r <romainzanchi30@gmail.com>
2014-12-23 14:02:33 -0800

    update website initialization script

--- website/start-website.sh
+++ website/start-website.sh
@@ -2,4 +2,5 @@
 
 python /home/website/lp_website/manage.py migrate
 python /home/website/lp_website/manage.py migrate static_precompiler
+python /home/website/lp_website/manage.py shell < /home/website/lp_website/init.py
 python /home/website/lp_website/manage.py runserver 0.0.0.0:8000
